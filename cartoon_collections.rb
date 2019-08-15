7185a985865b08536aa5ebff294c3c2b7c12057c
Laura Nickerson <laura.nickerson@gmail.com>
2019-08-15 22:51:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(dwarves)
   array = ["Doc", "Dopey", "Bashful", "Grumpy"]
-  array.
+  array.each_with_index do |array|
+    puts "#{array}"
 end
 
 def summon_captain_planet# code an argument here
